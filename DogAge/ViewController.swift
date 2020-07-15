95c7baa0e7f7f7a1ff4ee041c5c326aabe71e3c7
Daniel Sousa <daniel.sousa.developer@gmail.com>
2020-07-15 16:39:18 -0300

    first commit

--- DogAge/ViewController.swift
+++ DogAge/ViewController.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var inputValue: UITextField!
+    
+    @IBOutlet weak var agelabel: UILabel!
+    
+    @IBAction func whatsAge(_ sender: Any) {
+        let result = Int(inputValue.text!)! * 7;
+        agelabel.text = "A idade do seu cachorro em anos humanos é " + String(result);
+    
+    }
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
